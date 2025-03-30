dec906f37a308656d43c2b2302671d508721c655
Sol Milleding <solmilleding@gmail.com>
2025-03-30 08:47:50 +0200

    Set default ltex lang

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -229,6 +229,13 @@ return {
           },
         },
         bashls = {},
+        ltex = {
+          settings = {
+            ltex = {
+              language = 'en-US',
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
